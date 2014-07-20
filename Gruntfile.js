3fb9036e1a622342be322ecad66ac2c36c98dd2d
Grímur Daníelsson <grimur.danielsson@gmail.com>
2014-07-20 17:48:02 +0000

    Buildfile changes.

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function (grunt) {
+    'use strict';
+    // Project configuration
+    grunt.initConfig({
+        // Metadata
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+            ' Licensed <%= props.license %> */\n',
+        // Task configuration
+        concat: {
+            options: {
+                banner: '<%= banner %>',
+                stripBanners: true
+            },
+            dist: {
+                src: ['lib/ng imgAreaSelect.js'],
+                dest: 'dist/ng imgAreaSelect.js'
+            }
+        },
+        uglify: {
+            build: {
+            	files: {
+            		'public/ng-imgAreaSelect.min.js' : 'src/*ng-imgAreaSelect.js'
+            	}
+            }
+        },
+        jshint: {
+            options: {
+                node: true,
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: true,
+                eqnull: true,
+                browser: true,
+                globals: { jQuery: true },
+                boss: true
+            },
+            gruntfile: {
+                src: 'gruntfile.js'
+            },
+            lib_test: {
+                src: ['lib/**/*.js', 'test/**/*.js']
+            }
+        },
+        qunit: {
+            files: ['test/**/*.html']
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            lib_test: {
+                files: '<%= jshint.lib_test.src %>',
+                tasks: ['jshint:lib_test', 'qunit']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task
+    grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+};
+
--- src/ng-imgAreaSelect.js
+++ src/ng-imgAreaSelect.js
@@ -3,31 +3,41 @@
 angular.module('ng-imgAreaSelect', [])
 .directive('ngImgAreaSelect', function () {
     return {
-      restrict: 'AE',
+      restrict: 'A',
       scope: { 
       	selection: '=',
       	ngImgAreaSelect: '='
       },
       link: function postLink(scope, element, attrs) {
+        if (element[0].tagName== 'IMG') {
+        //do your stuff
+        	if(scope.selection === undefined) {
+        		scope.selection = {};
+        	}
+        	if(scope.ngImgAreaSelect === undefined) {
+        		scope.ngImgAreaSelect = {};
+        	}
+          scope.ngImgAreaSelect.instance = true;
+        	scope.ngImgAreaSelect.onSelectChange = function(img, selection) {
+        		scope.selection.x1 = selection.x1;
+        		scope.selection.x2 = selection.x2;
+        		scope.selection.y1 = selection.y1;
+        		scope.selection.y2 = selection.y2;
+        		scope.selection.width = selection.width;
+        		scope.selection.height = selection.height;
+        		scope.$apply();
+          };
+          
 
-      	if(scope.selection === undefined) {
-      		scope.selection = {};
-      	}
-      	if(scope.ngImgAreaSelect === undefined) {
-      		scope.ngImgAreaSelect = {};
-      	}
-
-      	scope.ngImgAreaSelect.onSelectChange = function(img, selection) {
-    		scope.selection.x1 = selection.x1;
-    		scope.selection.x2 = selection.x2;
-    		scope.selection.y1 = selection.y1;
-    		scope.selection.y2 = selection.y2;
-    		scope.selection.width = selection.width;
-    		scope.selection.height = selection.height;
-    		scope.$apply();
-        };
-
-        angular.element(element).imgAreaSelect(scope.ngImgAreaSelect);
-      }
+          var imgArea = angular.element(element).imgAreaSelect(scope.ngImgAreaSelect);
+          
+          scope.$watch('selection', function() {
+            imgArea.setSelection(scope.selection.x1, scope.selection.y1, scope.selection.x2, scope.selection.y2);
+            imgArea.update();
+          }, true);
+        } else {
+          console.log("ng-imgAreaSelection attribute can only be used on img elements.");
+        }
+      } 
     };
   });
